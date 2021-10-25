a2956271c3ccad38ae1d491b7e7dc9979991d41e
olzhas <toleugazin0@gmail.com>
2021-10-25 03:24:47 +0200

    ex 4.1 done

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import sqlite3
-import numpy as np
-import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates as dates
+
+from dateutil import parser
+
+
+
+import plotly.graph_objs as go
 import csv
-con = sqlite3.connect("loldb.db")
+con = sqlite3.connect("newdb.db")
 cur = con.cursor()
 tables = cur.execute("SELECT name FROM sqlite_master WHERE type = 'table'")
 for table in (tables):
@@ -21,6 +28,43 @@ cur.executemany("INSERT INTO indexInfo VALUES (?, ?, ?, ?)", rows1)
 cur.executemany("INSERT OR IGNORE INTO infoProcessed VALUES (?,?, ?, ?, ?, ?, ?, ?, ?)", rows2)
 
 
-cur.execute("SELECT indexData.Indexes, infoProcessed.CloseUSD FROM indexData LEFT JOIN infoProcessed ON indexData.Indexes = infoProcessed.Indexes")
-print(cur.fetchall())
+#cur.execute("create view v_notprocessed as select indexData.Indexes, indexData.Open from indexData left join infoProcessed on infoProcessed.Indexes=indexData.Indexes and infoProcessed.Date=indexData.Date where infoProcessed.Date is null;")
+#cur.execute("create view v_processed as select indexData.Date, indexData.Indexes, indexData.Open, indexData.Low, indexInfo.Exchange, indexInfo.Currency from indexData left join indexInfo on indexData.Indexes = indexInfo.Indexes group by indexData.Date")
+
+first_graph = cur.execute("select Open, Date from v_processed where Indexes = 'N225' limit 20")
+Open = []
+Date = []
+for row in first_graph:
+    Open.append(row[0])
+    Date.append(parser.parse(row[1]))
+
+
+fig, ax = plt.subplots()
+
+
+delta = datetime.timedelta(days=30)
+
+fig.autofmt_xdate()
+plt.title('figure 1', fontweight ="bold")
+ax.plot_date(Date,Open,'-')
+ax.xaxis.set_major_formatter(dates.DateFormatter('%Y-%m'))
+plt.show()
+
+second_graph = cur.execute("select Low, Date from v_processed where Indexes = 'N225' limit 20")
+
+Low = []
+Date = []
+for row in first_graph:
+    Low.append(row[0])
+    Date.append(parser.parse(row[1]))
+
+
+fig, ax = plt.subplots()
+ax.plot_date(Date,Low,'-')
+ax.xaxis.set_major_formatter(dates.DateFormatter('%Y-%m'))
+fig.autofmt_xdate()
+plt.title('figure 2', fontweight ="bold")
+plt.show()
+
 con.commit()
+con.close()
